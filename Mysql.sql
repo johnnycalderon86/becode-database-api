1af4a4e79481185d801641ffebc62737c7bdb6d0
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-08 14:53:41 +0200

    fixing my sql based on the comments from my colleagues

--- Mysql.sql
+++ Mysql.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE Notes_DB;
 CREATE TABLE notes_table ( 
-     AuthorName VARCHAR(100) NOT NULL , 
-     Title VARCHAR(100) NOT NULL ,
+      ID INT NOT NULL,
+      AuthorName VARCHAR(100) NOT NULL , 
+      Title VARCHAR(100) NOT NULL ,
       Note VARCHAR(500) NOT NULL ,
-)
-       
+      PRIMARY KEY (ID)
+);
